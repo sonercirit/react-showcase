b0a1e684984478f6b57a5cb07258742e82027c4c
sonercirit <sonercirit@gmail.com>
2021-04-12 16:45:46 +0300

    updates state getter for mockup pages

--- src/data/data.ts
+++ src/data/data.ts
@@ -2,5 +2,7 @@ import store from "./redux";
 
 export default function getData() {
   const state = store.getState();
-  return state.entries.filter((val) => val.releaseYear >= 2010).splice(0, 21);
+  return state.feed.series
+    .filter((val) => val.releaseYear >= 2010)
+    .splice(0, 21);
 }
