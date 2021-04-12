9a2b0c31df96e51bed3eb8fc393a32214e4c565f
sonercirit <sonercirit@gmail.com>
2021-04-12 09:43:45 +0300

    adds footer component to Main

--- src/Main.test.tsx
+++ src/Main.test.tsx
@@ -2,11 +2,18 @@ import { shallow } from "enzyme";
 import React from "react";
 import Main from "./Main";
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 describe("Main", () => {
   it("should have header", () => {
     const render = shallow(<Main />);
     const header = render.find(Header);
     expect(header.length).toEqual(1);
+  });
+
+  it("should have footer", () => {
+    const render = shallow(<Main />);
+    const footer = render.find(Footer);
+    expect(footer.length).toEqual(1);
   });
 });
